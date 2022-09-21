5461d97c00bae1196a179f8bc0665ad20a325a1f
Gavin Veitch <gavintveitch@gmail.com>
2022-09-21 15:47:15 -0600

    commit tree 11602e61e7dd1880abc8e5797fb399f86c30d89a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove()
+
+
+let newHeader = document.createElement("h1");
+    newHeader.textContent = "Bob is the champion";
+    newHeader.setAttribute(`id`, `victory`);
+    document.body.append(newHeader);
